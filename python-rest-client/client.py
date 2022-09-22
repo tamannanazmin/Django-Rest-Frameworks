b9722dd49718184713d933dcb066d7277e43ab5f
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-09-22 17:49:56 +0600

    posting data from client(not completed

--- python-rest-client/client.py
+++ python-rest-client/client.py
@@ -3,21 +3,40 @@ import requests
 '''url ="http://127.0.0.1:8000/api/users_list/"
 response=requests.get(url)
 print(response.text)'''
-
+URL= "http://127.0.0.1:8000"
 def get_token():
     #Get_auth token
-    url ="http://127.0.0.1:8000/api/auth/"
+    url =f"{URL}/api/auth/"
     response=requests.post(url,data={'username':'tamanna',
                                  'password': '123456'})
-    return response.json()
+    response.raise_for_status()  # raises exception when not a 2xx response
+    if response.status_code != 204:
+        return response.json()
+    #return response.json()
 #get_token()
 
 def get_data():
-    url="http://127.0.0.1:8000/api/user_list/"
+    url=f"{URL}/api/user_list/"
     header= {'Authorization' : f'Token{get_token()}'}
-    response = requests.get(url,headers=header)
-    #emp_data=response.json()
-    #for e in emp_data:
-     #   print(e)
-    print(response.text)
+    response = requests.get(url, headers=header)
+    response.raise_for_status()  # raises exception when not a 2xx response
+    if response.status_code != 204:
+        emp_data = response.json()
+        for e in emp_data:
+            print(e)
+    #print(response.json())
 get_data()
+
+def create_new(count):
+    url=f"{URL}/api/user_list/"
+    header= {'Authorization': f'Token{get_token()}'}
+    data={
+        "employee_id": f"HQ00{count}",
+        "name": "Raziv",
+        "age": 28,
+        "ranking": 2.4
+    }
+    response=requests.post(url, data=data, headers=header)
+    print(response.text)
+for e in range(20):
+    create_new(e)
